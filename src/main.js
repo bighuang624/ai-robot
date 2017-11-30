79fa7a73ecde83a25ab2dbe2d64cf62b42bb2873
bighuang624 <kyonhuang@qq.com>
2017-11-30 16:05:40 +0800

    :sparkles:import useful plugins

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import { Button, Select } from 'element-ui'
 import App from './App'
 
 Vue.config.productionTip = false
+
+Vue.use(Button)
+Vue.use(Select)
 
 /* eslint-disable no-new */
 new Vue({
